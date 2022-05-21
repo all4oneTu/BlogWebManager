01244542a3cf3c11059c6125ee6cd6f05b041c63
all4oneTu <81431586+all4oneTu@users.noreply.github.com>
2022-05-21 19:55:45 +0700

    update code 755

--- .history/pages/api/auth/[...nextauth]_20220521193933.js
+++ .history/pages/api/auth/[...nextauth]_20220521193933.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import RedditProvider from "next-auth/providers/reddit";
-
+import DiscordProvider from "next-auth/providers/discord";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
